ee35a83c948dc8d2a64bf3b1c178c568a796ea20
russell-j-boag <russell.boag@research.uwa.edu.au>
2016-09-28 15:15:29 +0800

    updated config files

--- data.analysis/atc.nasa-tlx.R
+++ data.analysis/atc.nasa-tlx.R
@@ -1,19 +1,59 @@
+###############################################################################
+# Clear workspace
 rm(list = ls())
 
+# Set working directory
+setwd("~/GitHub/phd/data.analysis")
+
+# Load project
 library('ProjectTemplate')
 load.project()
 
-# library("plyr")
+###############################################################################
+# Create aggregate data objects
+df.nasa.tlx <- bind_rows(p.idx.atc.a1.pilot, p.idx.atc.a1.1)
 
-df <- rbind(p.idx.atc.a1.pilot, p.idx.atc.a1.1)
+# Check structure and prep data
+str(df.nasa.tlx[,1:16])
+df.nasa.tlx$exp.name <- factor(df.nasa.tlx$exp.name)
+df.nasa.tlx$p.id <- factor(df.nasa.tlx$p.id)
+df.nasa.tlx$p.gender <- factor(df.nasa.tlx$p.gender)
+df.nasa.tlx$p.handedness <- factor(df.nasa.tlx$p.handedness)
+df.nasa.tlx$p.pool <- factor(df.nasa.tlx$p.pool)
+df.nasa.tlx$key.map <- factor(df.nasa.tlx$key.map)
+str(df.nasa.tlx[,1:16])
 
-# remove empty rows
-df <- df[ which(df$p.gender!=""), ]
+# Remove empty rows
+df.nasa.tlx <- filter(df.nasa.tlx, p.gender != "")
 
-# remove rows flagged as 'EXCLUDE'
-df <- df[ which(df$p.exclude=="TRUE"), ]
-str(df)
-colnames(df)
+# Filter out observations tagged as 'EXCLUDE'
+df.nasa.tlx <- filter(df.nasa.tlx, p.exclude != "TRUE")
+
+###############################################################################
+# Describe sample
+describe.sample <- df.nasa.tlx %>%
+    group_by(exp.name) %>%
+        summarise(
+            N=sum(p.gender=="M")+sum(p.gender=="F"),
+            Age.Mean=round(mean(p.age,na.rm=TRUE),digits=2),
+            Age.Median=median(p.age,na.rm=TRUE),
+            Age.SD=round(sd(p.age,na.rm=TRUE),digits=2),
+            Age.Min=min(p.age,na.rm=TRUE),
+            Age.Max=max(p.age,na.rm=TRUE),
+            N.Male=sum(p.gender=="M"),
+            N.Female=sum(p.gender=="F"),
+            Female.Percent=
+                round(sum(p.gender=="F")/
+                    (sum(p.gender=="M")+
+                        sum(p.gender=="F"))*100,digits=1),
+            Right.Handed.Percent=
+                round(sum(p.handedness=="R")/
+                    (sum(p.handedness=="A")+
+                        sum(p.handedness=="L")+                                                                    sum(p.handedness=="R"))*100,digits=1)
+            )
+
+describe.sample
+
 
 # data subsets
 # a1 <- df[ which(df$exp.name=="A1"), ] # Experiment A1 data only (Neutral emphasis)
@@ -21,21 +61,6 @@ colnames(df)
 # a3 <- df[ which(df$exp.name=="A3"), ] # Experiment A3 data only (On-time emphasis)
 # a4 <- df[ which(df$exp.name=="A4"), ] # Experiment A4 data only (PM emphasis)
 
-describe.sample <- ddply(df, ~key.map,
-                         summarise,
-                         N=sum(p.gender=="M")+sum(p.gender=="F"),
-                         Age.Mean=round(mean(p.age,na.rm=TRUE),digits=2),
-                         Age.Median=median(p.age,na.rm=TRUE),
-                         Age.SD=round(sd(p.age,na.rm=TRUE),digits=2),
-                         Age.Min=min(p.age,na.rm=TRUE),
-                         Age.Max=max(p.age,na.rm=TRUE),
-                         N.Male=sum(p.gender=="M"),
-                         N.Female=sum(p.gender=="F"),
-                         Female.Percent=round(sum(p.gender=="F")/(sum(p.gender=="M")+sum(p.gender=="F"))*100,digits=1),
-                         Right.Handed.Percent=round(sum(p.handedness=="R")/(sum(p.handedness=="A")+sum(p.handedness=="L")+sum(p.handedness=="R"))*100,digits=1)
-)
 
-describe.sample
 
-# check sample characteristics are similar when split on factor
-
+ggsave(file.path('graphs', 'plot1.pdf'))
--- todo.md
+++ todo.md
@@ -1,47 +1,57 @@
-# To Do...
+# To Do:
 
 ## UTAS Trip December 7th to 21st
 
-- Confirm details with AH
-- Book flights
+- Anna to book flights and accommodation
 - Book Melbourne accommodation for NYE
 
 
 ## AMPC 2017
+
 - Write abstract
 - Submit application
 
 
+## ATC Experiment 2.0
 
-## ATC Experiment 2.0:
+- [x] Make data extraction script label responses as 'RESPONSE', 'NONRESP.SEEN', and 'NONRESP.UNSEEN', under 'Response.Type' factor
+- [x] Adapt to run on 2 vs 5 aircraft per trial blocks
+- [x] Make script calculate 'seen' time for Nonresp.SEEN T.Seen <- (Trial.Duration - sum(RT.1, RT.2, etc.))
+- [ ] Run nonrespone labeller and seen-time calculator on real data
 
-- Find NASA-TLX forms for EXCLUDED participants and mark with X
+- [ ] Get data extraction script to pull line numbers from raw and rank(x) them in new variable
 
 - Re-extract PILOT data from raw
 
 - Re-combine individual subject data into group data file for A1.1
 
-- Email week 7 IO participant details to SL
-- Email week 8 IO participant details to SL
-- Email week BREAK IO participant details to SL
-- Email week 9 IO participant details to SL
-- Email week 10 IO participant details to SL
 
-- Find AV instructions in PACT.zip
+- Email week BREAK IO participant details to SL
+- Remind week 9 participants 
+- Email week 9 IO participant details to SL
+- Remind week 10 participants
+- Email week 10 IO participant details to SL
+- Remind week 11 participants
+- Email week 11 IO participant details to SL
+- Export WMC data
 
 - Print NASA-TLX x75
 
+
+## General Housekeeping:
 - Fast-file-rename raw logs for PILOT
 - Fast-file-rename raw logs for A1.1
 - Fast-file-rename raw logs for A1, A2, A3, A4
 
+- Enroll in MATH1001
+
 
 ## PhD:
-- Fix Markdown on PhD.Proposal document
+- Fix Markdown on PhD Proposal document
 
 
 ## Machine Learning:
-- Test Weka
+- Weka
 
 
 ## Add to Open Source Guide:
@@ -50,7 +60,8 @@
 - PuTTY
 - tmux
 
+## Ebay: 
+- Soundproofing/Acoustic baffles/foam
 
-## Google Drive:
-- Tidy up atc folder names
+
 
